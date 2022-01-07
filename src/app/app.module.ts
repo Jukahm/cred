8137b87107019906caf3fbd7fcfbffca57d2d02d
Juliano Martins <julianoahm@gmail.com>
2022-01-07 11:48:31 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { VehicleItemComponent } from './components/vehicle-item/vehicle-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiclesComponent,
+    VehicleItemComponent
   ],
   imports: [
     BrowserModule,
